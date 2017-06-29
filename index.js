57a605424cefa6719de534991317a6fd83191f22
Tian Qi Xiong <tianqi.xiong@gmail.com>
2017-06-29 02:00:24 -0400

    mod

--- index.js
+++ index.js
@@ -3,75 +3,64 @@ window.onbeforeunload = function () {
 	window.scrollTo(0, 0);
 }
 
+//____POPULATE TOUR SECTION_______
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://rest.bandsintown.com/artists/japanesebreakfast/events?app_id=japanesebreakfastofficialsite", true);
+xhr.onload = function (e) {
+  if (xhr.readyState === 4) {
+    if (xhr.status === 200) {
+    	var response = JSON.parse(xhr.responseText);
+		for(i=0; i<response.length; i++){
+			var event = create_event_row(response[i]);
+			$('#tour-container').append(event);
+		}
+    } else {
+      console.error(xhr.statusText);
+    }
+  }
+};
+xhr.onerror = function (e) {
+  console.error(xhr.statusText);
+};
+xhr.send(null);
+$('#tour-container').find('.row').last().find('.evt').css('border-bottom', 'none');
+
 $('html, body').hide();
-var first_img_load = true;
-
-//_____GET YOUTUBE PLAYER_______
-var tag = document.createElement('script');
-tag.id = 'iframe-demo';
-tag.src = 'https://www.youtube.com/iframe_api';
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var window_height;
-var window_width;
 var header_nav_height;
-var nav_toggler_shown;
 var last_clicked;
+var social_icon_class = 'fa-lg';
+var play_icon_class = 'fa-3x';
 var first_click = true;
-var first_load = true;
-var home_img_height;
 
 $(document).ready(function() {
-	window_width = $(window).width();
-	window_height = $(window).height();
-	//___SET HEADER NAV HEIGHT______
-	console.log("h: "+$(window).height());
-	console.log("w: "+$(window).width());
-	$('#header-nav').height( window_height * .18934911 );
-	header_nav_height = $('#header-nav').height();
-	$('#footer-nav').height( window_height * .16715976 );
-	$('.custom-nav').css('padding', (window_width * .03868472).toString() );
 
-	//____POPULATE TOUR SECTION_______
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "https://rest.bandsintown.com/artists/japanesebreakfast/events?app_id=japanesebreakfastofficialsite", true);
-	xhr.onload = function (e) {
-	  if (xhr.readyState === 4) {
-	    if (xhr.status === 200) {
-	    	var response = JSON.parse(xhr.responseText);
-			for(i=0; i<response.length; i++){
-				var event = create_event_row(response[i]);
-				$('.tour-container').append(event);
-			}
-	    } else {
-	      console.error(xhr.statusText);
-	    }
-	  }
-	};
-	xhr.onerror = function (e) {
-	  console.error(xhr.statusText);
-	};
-	xhr.send(null);
+	//___SET HEADER NAV HEIGHT______
+	set_navs();
 
 	//___SET VIEWS______
 	$('html, body').fadeIn(300, 'swing');
-	if ( $('#custom-navbar-toggler').is(":visible") ){
-		$('#header-nav').removeClass('fixed-top');
-		nav_toggler_shown = true;
-	} else {
-		nav_toggler_shown = false;
-	}
 
 	//____TRIGGER HOME PAGE________
 	setTimeout(function(){
 		$('#home-btn').trigger('click');
 	}, 300);
 
-	//____SET EVENT HANDLERS_______
+	//____CLICK HANDLERS_____
 	$('#brand-header').click(function(e) {
 		e.preventDefault();
-		$('#home-btn').trigger('click');
+		if (!nav_toggler_shown()){	
+			$('#home-btn').trigger('click');
+		}
+	});
+
+	$('#custom-navbar-toggler').click(function(){
+		first_click = false;
+	});
+
+	$('.navbar-collapse').click(function(){
+		if ( first_click == false ){
+			$('#custom-navbar-toggler').trigger('click');
+		}
 	});
 
 	$('.nav-item').click( function(e) {
@@ -82,8 +71,8 @@ $(document).ready(function() {
 
 		var name = $(this).attr('data-name');
 		if ( last_clicked != name ) {
-			if ($('#body-content .section').length > 0) {
-					$('#body-content .section').fadeOut( 300, 'swing', function() {
+			if ($('#body-content .active').length > 0) {
+					$('#body-content .active').fadeOut( 300, 'swing', function() {
 						window.scrollTo(0, 0);
 						switch_content( name );
 					});
@@ -123,158 +112,157 @@ $(document).ready(function() {
 		$('.player').height('100%');
 	});
 
+	// SET WINDOW RESIZE EVENT HANDLERS
 	$(window).resize(function(){
-		if(last_clicked == 'video'){
-			set_video_overlay();
-		}
-	});
+		setTimeout(function(){
+			if ($(window).width() < 615) {
+				$('#brand-header').css('font-size', '.8em');
+				$('.custom-nav').css('padding', '15px');
+			}else{
+				$('#brand-header').css('font-size', '2em');
+				$('.custom-nav').css('padding', '43px');
+			}
 
-	//____SET SMALL SCREEN EVENT HANDLERS_________
-	if (window_width < 500) {
-		$('#brand-header').css('font-size', '.8em');
-		$('.custom-nav').css('padding', '15px 15px 15px 15px');
-		$('.custom-nav .container').css('width', '95%');
-		$('.custom-nav .container').css('padding-left', '5%');
-	}
+			// HANDLE NAV TOGGLER
+			if ( nav_toggler_shown() ){
+				$('#home-btn').show();
+				$('#home-btn').removeAttr('hidden');
+				$('#header-nav').removeClass('fixed-top');
+				$('#header-nav').height('auto');
+				$('#body-content').css('min-height', ($(window).height() - $('#header-nav').outerHeight()).toString()+'px');
+				if( $('#custom-navbar-toggler').outerHeight(true) > $('#header-nav .container').height() ){
+					$('#custom-navbar-toggler').height( $('#header-nav .container').height() );
+					$('.navbar-toggler-icon').height( $('#custom-navbar-toggler').height() );
+				}
+				$('#body-content').css('padding-top', '0');
+				$('#footer-nav .navbar-text').css('font-size', '.5em');
+			}else{
+				$('#home-btn').hide();
+				$('#header-nav').addClass('fixed-top');
+				$('#body-content').css('min-height', $(window).height().toString()+'px');
+				// set_navs();
+				$('#header-nav').height(header_nav_height);
+				$('#body-content').css('margin-top', '0');
+				$('#body-content').css('padding-top', header_nav_height);
+				$('#footer-nav .navbar-text').css('font-size', '.8em');
+			}
 
-	// IF TOGGLER IS VISIBLE
-	if ( $('#custom-navbar-toggler').is(":visible") ){
-		var toggler_height = $('#custom-navbar-toggler').outerHeight(true);
-		if( toggler_height > $('#header-nav .container').height() ){
-			$('#custom-navbar-toggler').height( $('#header-nav .container').height() );
-			$('.navbar-toggler-icon').height( $('#custom-navbar-toggler').height() );
-		}
-		$('#header-nav').height('auto');
-		$('#body-content').css('padding-top', '0');
-		$('#footer-nav .navbar-text').css('font-size', '.5em');
-		$('#social-row').width($('#body-content .section').width());
-		$('#social-row .social').removeClass('text-right').addClass('text-center');
+			if (last_clicked == 'home'){
+				setHome();
+			}
 
-		$('#custom-navbar-toggler').click(function(){
-			first_click = false;
-		});
+			if (last_clicked == 'video'){
+				setVideo();
+			}
 
-		$('.navbar-collapse').click(function(){
-			if ( first_click == false ){
-				$('#custom-navbar-toggler').trigger('click');
+			if (last_clicked == 'music'){
+				setMusic();
+			}
+
+			if (last_clicked == 'shop'){
+				setShop();
+			}
+
+			if( $(window).height() > $('#body-content .active').height() ){
+				$('#body-content').addClass('vertical-center');
+			}else{
+				$('#body-content').removeClass('vertical-center');
 			}
 		});
-	}
+	});
 });
 
+function set_navs(){
+	// $('#header-nav').height( $(window).height() * .18934911 );
+	// header_nav_height = $('#header-nav').height();
+	$('#header-nav').height( 111 );
+	header_nav_height = $('#header-nav').height();
+	$('#footer-nav').height( $(window).height() * .16715976 );
+	// $('.custom-nav').css('padding', ($(window).width() * .03868472).toString()+'px' );
+	$('.custom-nav').css('padding', '43px' );
+}
+
 function switch_content( content_name ) {
-	$('#body-content .section').hide();
-	$('#body-content').empty();
-	$('.' + content_name + '-container').clone(true, true).appendTo($('#body-content'));
-	$('#body-content .' + content_name + '-container').removeAttr('hidden');
+	$('#body-content .active').hide();
+	$('#body-content .active').removeClass('active');
+	$('#' + content_name + '-container').addClass('active');
+	$('#' + content_name + '-container').removeAttr('hidden');
 
 	// SHOW BODY
-	$('#body-content .' + content_name + '-container').hide().fadeIn( 300, 'swing');
+	$('#' + content_name + '-container').hide().fadeIn( 300, 'swing');
 
-	// SET DEFAULTS
-	if (nav_toggler_shown == false){
-		$('#header-nav').height(header_nav_height);
-		$('#body-content').css('margin-top', '0');
-		$('#body-content').css('padding-top', header_nav_height);
-		$('#header-nav').addClass('fixed-top');
-	}
+	$(window).trigger('resize');
 
-	if ( nav_toggler_shown ){
-		$('#body-content').css('min-height', (window_height - $('#header-nav').outerHeight()).toString()+'px');
-	}else{
-		$('#body-content').css('min-height', window_height.toString()+'px');
-	}
-
-	if(window_height > $('#body-content .section').height()){
-		$('#body-content').addClass('vertical-center');
-	}else{
-		$('#body-content').removeClass('vertical-center');
-	}
-
-	// HOME
-	if(content_name == 'home'){
-		if (first_load){
-			first_load = false;
-			var before_icon_height = 21;
-			$('#social-row').width($('#body-content .section').width());
-
-			$('.social i').width( window_width * .03125 );
-			$('.social i').height( window_width * .03125 );
-			$('.social i').css('font-size', (window_width * .0390625 / 2).toString() );
-			if($(window).width() > 1500){
-				before_icon_height = 32;
-				var new_class;
-				if($(window).width() > 2000){
-					before_icon_height = 48;
-					new_class = 'fa-3x';
-					$('.social i').css('border-width', '4px');
-				}else{
-					new_class = 'fa-2x';
-				}
-				$('.social i').each(function(){
-					$(this).removeClass('fa-lg').addClass(new_class);
-				});
-			}
-			$('.social i').css('padding-top', (($('.social i').height() + 4 - before_icon_height)/2).toString()+'px');
-			$('.social ul li').css('margin', ($('.social i').width() / 20).toString() + 'px' );
-			$('#home-img').height( window_height - $('#header-nav').outerHeight(true) - $('#social-row').outerHeight(true));
-			home_img_height = $('#home-img').height();
-		}else{
-			$('#home-img').height( home_img_height );
-		}
-	}
-
-	// TOUR
-	if(content_name == 'tour'){
-		$('#body-content .tour-container').find('.row').last().find('.evt').css('border-bottom', 'none');
-	}
-
-	// MUSIC
-	if(content_name == 'music'){
-		if($('#body-content .section').width() < 600 ){
-			$('#ssfap-spotify-wrapper').find('iframe').replaceWith('<iframe src="https://open.spotify.com/embed?uri=spotify%3Atrack%3A7HXfMFsrVmUQqjhBuLsZtY" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>');
-			$('#psychopomp-spotify-wrapper').find('iframe').replaceWith('<iframe src="https://open.spotify.com/embed?uri=spotify%3Aalbum%3A3i7EHinpu9J5MXKMzjpjZ0&theme=white&view=coverart" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>')
-		}
-	}
-
-	// VIDEO
-	if(content_name == 'video'){
-		set_video_overlay();
-		if (nav_toggler_shown == false){
-			$('#header-nav').removeClass('fixed-top');
-			$('#header-nav').outerHeight(80);
-			$('#body-content').css('margin-top', '-20px');
-			$('#body-content').css('padding-top', '0');
-		}else{
-			$('.info').show();
-			$('.info').css('background-color', 'rgba(0,0,0,0)');
-			var small_icon = true;
-			if( window_width > 500){
-				small_icon = false;
-			}
-			$('.fa-play').each(function(){
-				if (small_icon){
-					$(this).removeClass('fa-3x');
-				}else{
-					$(this).removeClass('fa-3x').addClass('fa-2x');
-				}
-			});
-		}
+	switch( content_name ){
+		case 'home':
+			load_home();
+			return
+		case 'tour':
+			load_tour();
+			return
+		case 'music':
+			load_music();
+			return
+		case 'video':
+			load_video();
+			return
+		case 'shop':
+			load_shop();
+			return
+		case 'contact':
+			load_contact();
+			return
 	}
 }
 
-function set_video_overlay(){
-	$('.overlay').each(function(){
-		if ($(this).find('img').length != 0){
-			$(this).height($(this).find('img').height());			
-		} else {
-			$(this).height($(this).find('iframe').height());
-		}
-	});
+function nav_toggler_shown(){
+	if ( $('#custom-navbar-toggler').is(":visible") ){
+		return true;
+	} else {
+		return false;
+	}
+}
 
-	$('.overlay .info').each(function(){
-		$(this).css('line-height', $(this).height().toString() + 'px');
-	});	
+//_____HOME
+function load_home(){
+}
+
+function setHome(){
+	if ($(window).width() < 1000){
+		$('#social-row').closest('div').removeClass('text-right').addClass('text-center');
+	}else{
+		$('#social-row').closest('div').removeClass('text-center').addClass('text-right');
+	}
+
+	if ($(window).width() > 1500){
+		if ($(window).width() > 2000){
+			setSocial('fa-3x');
+		}else{
+			setSocial('fa-2x');
+		}
+	}else{
+		if ($(window).width() < 670){
+			setSocial('');
+		}else{
+			setSocial('fa-lg');
+		}
+	}
+
+	if ($(window).height() > 500){
+		$('#home-img').height( ($(window).height() - header_nav_height - $('#social-row').outerHeight(true)) * .9 );
+	}
+}
+
+function setSocial( new_class ){
+	$('.fa-stack').each(function(){
+		$(this).removeClass(social_icon_class).addClass(new_class);
+	});
+	social_icon_class = new_class;
+}
+
+//_____TOUR
+function load_tour(){
+	$('#body-content').removeClass('text-center');
 }
 
 function create_event_row( event ){
@@ -305,7 +293,21 @@ function create_event_row( event ){
 		offerlink = event.offers[0].url;
 	}
 
-	var append_string = '<div class="row"><div class="col-lg-1 evt event-date">'+date+'</div><div class="col-lg-1 evt event-weekday">'+weekday+'</div><div class="col-lg-3 evt event-name">'+name+'</div><div class="col-lg-2 evt event-location">'+location+'</div><div class="col-lg-5 evt event-links">';
+	var time = 	'<div class="time">'
+				+'<div class="evt event-date">'+date+'</div>'
+				+'<div class="evt event-weekday">'+weekday+'</div>'
+				+'</div>';
+
+	var venue_name = 	'<div class="venue_name">'
+						+'<div class="evt event-name">'+name+'</div>'
+						+'</div>';
+
+	var venue_location = 	'<div class="venue_location">'
+							+'<div class="evt event-location">'+location+'</div>'
+							+'</div>';
+
+	var links = '<div class="evt event-links"><a href="'+rsvplink+'" target="_blank" class="custom-btn">RSVP</a>';
+
 	if (offer == undefined || offer.toLowerCase() == 'sold out'){
 		var offer_text;
 		if (offer != undefined){
@@ -313,12 +315,17 @@ function create_event_row( event ){
 		}else{
 			offer_text = 'tickets';
 		}
-		append_string += '<a><button type="button" class="btn btn-outline-secondary custom-btn empty">'+offer_text.toUpperCase()+'</button></a>';
+		links += '<a class="custom-btn empty">'+offer_text.toUpperCase()+'</a></div>';
 	}else{
-		append_string += '<a href="'+offerlink+'" target="_blank"><button type="button" class="btn btn-outline-secondary custom-btn">'+offer.toUpperCase()+'</button></a>';
+		links += '<a href="'+offerlink+'" target="_blank" class="custom-btn">'+offer.toUpperCase()+'</a></div>';
 	}
-	append_string += '<a href="'+rsvplink+'" target="_blank"><button type="button" class="btn btn-outline-secondary custom-btn">RSVP</button></a></div></div>';
 
+	var append_string = '<div class="row">'
+						+ time
+						+ venue_name
+						+ venue_location
+						+ links
+						+'</div>';
 	return append_string
 }
 
@@ -356,6 +363,154 @@ function getWeekDay( year, month, day) {
 	var days = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
 
 	return days[date.getDay()]
+}
+
+//_____MUSIC
+function load_music(){
+
+}
+
+function setMusic(){
+	if( $(window).width() < 750 ){
+		if ( $('.spotify-wrapper iframe').height() > 100){
+			$('#ssfap-spotify-wrapper').find('iframe').replaceWith('<iframe src="https://open.spotify.com/embed?uri=spotify%3Atrack%3A7HXfMFsrVmUQqjhBuLsZtY" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>');
+			$('#psychopomp-spotify-wrapper').find('iframe').replaceWith('<iframe src="https://open.spotify.com/embed?uri=spotify%3Aalbum%3A3i7EHinpu9J5MXKMzjpjZ0&theme=white&view=coverart" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>');
+		}
+	}else{
+		if ( $('.spotify-wrapper iframe').first().height() < 100 ){
+			$('#ssfap-spotify-wrapper').find('iframe').replaceWith('<iframe src="https://open.spotify.com/embed?uri=spotify:track:7HXfMFsrVmUQqjhBuLsZtY" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>');
+			$('#psychopomp-spotify-wrapper').find('iframe').replaceWith('<iframe src="https://open.spotify.com/embed?uri=spotify%3Aalbum%3A3i7EHinpu9J5MXKMzjpjZ0&theme=white" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>');			
+		}
+	}
+}
+
+//_____VIDEO
+function load_video(){
+	//_____GET YOUTUBE PLAYER_______
+	var tag = document.createElement('script');
+	tag.id = 'iframe-demo';
+	tag.src = 'https://www.youtube.com/iframe_api';
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+	//_____SET VIDEOS_______
+	var img = $('.overlay').first().find('img').first();
+	var img_height = img.height();
+	var img_width = img.width();
+	var body_height = $(window).height() - 100;
+	var multiply = 16 / 9;
+	if (img_height > body_height) {
+		$('#video-container').width( body_height * multiply );
+	}
+}
+
+function setVideo(){
+	set_video_overlay();
+	var vid_width = $('#video-container').width();
+	$('.video-wrapper').width( vid_width );
+	$('.video-wrapper').height( (vid_width * 9) / 16 );
+	if (nav_toggler_shown()){
+		$('.video .info').show();
+		$('.video .info').css('background-color', 'rgba(0,0,0,0)');
+		if( $(window).width() < 500){
+			$('.fa-play').each(function(){
+				$(this).removeClass(play_icon_class).addClass('');
+			});
+			play_icon_class = 'fa-2x';
+		}else{
+			$('.fa-play').each(function(){
+				$(this).removeClass(play_icon_class).addClass('fa-2x');
+			});
+			play_icon_class = '';
+		}
+
+		$('.video .overlay').each(function(){
+			$(this).off('mouseenter mouseleave');
+		});
+	}else{
+		$('#header-nav').removeClass('fixed-top');
+		$('#header-nav').css('padding-top', '0px' );
+		$('#header-nav').css('padding-bottom', '0px' );
+		$('#header-nav').height(80);
+		$('#body-content').css('margin-top', '-20px');
+		$('#body-content').css('padding-top', '0');
+
+		$('.video .info').hide();
+		$('.video .info').css('background-color', 'rgba(0,0,0,.5)');
+		$('.fa-play').each(function(){
+			$(this).removeClass(play_icon_class).addClass('fa-3x');
+		});
+		play_icon_class = 'fa-3x';
+
+		$('.video .overlay').each(function(){
+			$(this).hover(
+				function(){
+					$(this).find('.info').show();
+				},
+				function(){
+					$(this).find('.info').hide();
+				}
+			);
+		});
+	}
+}
+
+function set_video_overlay(){
+	var h;
+	$('.overlay').each(function(){
+		if ($(this).find('img').length != 0){
+			h = $(this).find('img').height();
+		} else {
+			h = $(this).find('iframe').height();
+		}
+		$(this).height( h );
+	});
+
+	$('.overlay .info').each(function(){
+		$(this).css('line-height', $(this).height().toString() + 'px');
+	});
+}
+
+//SHOP
+function load_shop(){
+
+}
+
+function setShop(){
+	set_shop_overlay();
+	if (nav_toggler_shown()){
+		$('.shop-info span').hide();
+		$('.shop-info').show();
+		$('.shop-info').css('background-color', 'rgba(0,0,0,0)');
+		$('.shop-overlay').each(function(){
+			$(this).off('mouseenter mouseleave');
+		});
+	}else{
+		$('.shop-info span').show();
+		$('.shop-info').hide();
+		$('.shop-info').css('background-color', 'rgba(0,0,0,.5)');
+		$('.shop-overlay').each(function(){
+			$(this).hover(
+				function(){
+					$(this).find('.shop-info').show();
+				},
+				function(){
+					$(this).find('.shop-info').hide();
+				}
+			);
+		});
+	}
+}
+
+function set_shop_overlay(){
+	$('.shop-overlay .shop-info').each(function(){
+		$(this).css('line-height', $(this).height().toString() + 'px');
+	});
+}
+
+//CONTACT
+function load_contact(){
+
 }
 
 
